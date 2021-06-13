7de2ade7cf60dce805602dcc7712fecbd4bac679
abhishekkumar116 <85789198+abhishekkumar116@users.noreply.github.com>
2021-06-13 13:12:38 +0530

    Add files via upload

---
+++ Bestinlist-python-bootcamp.py
@@ -0,0 +1,55 @@
+print("30 days 30 hour challange")
+print('30 days 30 hour challange')
+
+# String to variable
+
+Hours = "thirty"
+print(Hours)
+
+# indexding using string
+
+Days = "thirty days"
+print(Days[0])
+
+# print particular character from certain text
+
+challenge = "i will win"
+print(challenge[7:10])
+
+# print the length of character
+
+print(len(challenge))
+
+# covert string into lower character
+
+print(challenge.lower())
+
+# joining two string
+
+a = "30 Days"
+b = "30 hours"
+c = a + b
+print(c)
+
+# adding space during concatenation
+
+a = "30 Days"
+b = "30 hours"
+c = a + " " + b
+print(c)
+
+# casefold - Usage
+
+text = "Thirty days and Thirty hours"
+x = text.casefold()
+print(x)
+
+y = text.capitalize()
+print(y)
+
+a = text.isalpha()
+print(a)
+
+b = text.isalnum()
+print(b)
+
