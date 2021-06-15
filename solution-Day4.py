54b79a98a8d52301c9b7eb2254657bbdc7940cf2
abhishekkumar116 <85789198+abhishekkumar116@users.noreply.github.com>
2021-06-15 16:41:21 +0530

    Add files via upload

---
+++ solution-Day4.py
@@ -0,0 +1,26 @@
+# Solution of first problem
+
+a = b = c = int(input("Enter the value:"))
+
+d, e, f = 10, 50, 60
+
+print("The division is:", a % d)
+print("The multiplication is:", b*e)
+print("The addition is:", c + f)
+
+# Solution of second problem
+
+a = "Greek"
+print(a[ :2] + 'G' + a[3: ])
+
+
+# Solution of third problem
+
+a = int(input("Enter the integer value:"))
+
+b = float(input("Enter the float value:"))
+
+print(float(a))
+print(int(b))
+
+
